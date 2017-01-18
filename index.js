3dac8d76b7c2b56133f6b24a55aaaa029074c9e2
carlos4rias <carlos.23325@gmail.com>
2017-01-18 15:05:32 -0500

    creating the endpoints POST-GET-DELETE-PUT

--- index.js
+++ index.js
@@ -9,9 +9,27 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded( {extended: false} ))
 app.use(bodyParser.json())
 
+app.get('/api/product', (req, res) => {
+  //res.send(200, { products: []})
+  res.status(200).send({ products: []})
+})
 
-app.get('/test/:name', (req, res) => {
-  res.send({ message: `Hi ${req.params.name}!`})
+app.get('/api/product/:productId', (req, res) => {
+
+})
+
+app.post('/api/product', (req, res) => {
+  console.log(req.body)
+  //res.send(200, {message: 'The product has been received!'})
+  res.status(200).send( {message: 'The product has been received!'})
+})
+
+app.put('/api/product/:productId', (req, res) => {
+
+})
+
+app.delete('/api/product/:productId', (req, res) => {
+
 })
 
 app.listen(3000, () => {
