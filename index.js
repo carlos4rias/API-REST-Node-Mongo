37155ad3b70d76c35c7c4dc6d1d6dacc6fe93c6c
carlos4rias <carlos.23325@gmail.com>
2017-01-18 14:24:39 -0500

    testing the endpoint with GET, and getting a response

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded( {extended: false} ))
 app.use(bodyParser.json())
 
+
+app.get('/test/:name', (req, res) => {
+  res.send({ message: `Hi ${req.params.name}!`})
+})
+
 app.listen(3000, () => {
   console.log(`Server runing at http://localhost:${port}`)
 })
